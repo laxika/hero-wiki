cf2fd7a0f77c487425da899d70b2a8360495b592
Gyula Lakatos <v-glakatos@hotels.com>
2016-03-31 13:22:24 +0200

    First try for jar deploy

--- src/main/java/com/morethanheroic/HeroicWikiApplication.java
+++ src/main/java/com/morethanheroic/HeroicWikiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan
 @EnableAutoConfiguration
-public class HeroicWikiApplication extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
+public class HeroicWikiApplication extends WebMvcAutoConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(HeroicWikiApplication.class, args);
